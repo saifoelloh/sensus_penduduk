af64a75951b8c8febc9f6b1adb0a881afdca037a
saifoelloh <saifoelloh@gmail.com>
2020-01-06 22:49:54 +0700

    adding new layout into project

---
+++ app/src/main/java/com/example/sensuspenduduk/ListSensus.java
@@ -0,0 +1,14 @@
+package com.example.sensuspenduduk;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ListSensus extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_list_sensus);
+    }
+}
